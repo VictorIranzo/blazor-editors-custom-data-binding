6018ddc843a41c7399110c2043bb8d49ecc99d34
Victor Iranzo <victor.iranzo@burningbox.es>
2024-12-10 11:06:26 +0100

    Add grid dev extreme bug with footer template.

--- CS/WebApi/Controllers/DataController.cs
+++ CS/WebApi/Controllers/DataController.cs
@@ -27,6 +27,47 @@ namespace WebApi.Controllers
             return result;
         }
 
+        [HttpGet("GetCustomers")]
+        public async Task<ActionResult<LoadResult>> GetCustomers(DataSourceLoadOptions filter)
+        {
+            await Task.Delay(Random.Shared.Next(150, 300));
+
+            var data = new List<Customer>()
+            {
+                new Customer()
+                {
+                    Address = "507 - 20th Ave. E.Apt. 2A",
+                    City = "Seattle",
+                    CompanyName = "Alfreds Futterkiste",
+                    ContactName = "Maria Anders",
+                    ContactTitle = "Sales Representative",
+                    Country = "USA",
+                    Fax = "(206) 555-4111",
+                    Phone = "(206) 555-9857",
+                    PostalCode = "98122",
+                    Region = "WA",
+                    Total = 10,
+                },
+                new Customer()
+                {
+                    Address = "Obere Str. 57",
+                    City = "Berlin",
+                    CompanyName = "Around the Horn",
+                    ContactName = "Thomas Hardy",
+                    ContactTitle = "Sales Representative",
+                    Country = "Germany",
+                    Fax = "030-0076545",
+                    Phone = "030-0074321",
+                    PostalCode = "12209",
+                    Total = 5,
+                },
+            };
+
+            var result = DataSourceLoader.Load(data, filter);
+
+            return result;
+        }
+
         [HttpPost]
         public async Task<WebApiLookup> CreateAsync(WebApiLookup webApiLookup)
         {
